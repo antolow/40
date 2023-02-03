3286c69bff7d126d2e2613825ebb1c009980c2d3
antolow <98319496+antolow@users.noreply.github.com>
2023-02-03 18:05:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let myForm = document.forms.myForm;
+let elements = myForm.elements;
+let result = document.querySelector('.result');
+let btn = elements.btn;
+let str = elements.str;
+
+btn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    let masString = str.value.split('');
+    result.append(masString);
+    console.log(masString)
+
+    let masNum = []
+    length = masString.length
+    for (let i = 0; i < length; i++)
+        masNum.push(parseInt(masString[i]));
+    console.log(masNum)
+
+    let numbs;
+    mas.array.forEach(el => {
+        numbs.push(+elements)
+    })
+})
+
